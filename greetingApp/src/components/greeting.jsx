6f961bf8de29dacea68b5569c594add6a49f883b
Shubham Gaikwad <shubhamtechgeek@gmail.com>
2024-03-13 21:56:08 +0530

    using useEffect to store the greet

--- greetingApp/src/components/greeting.jsx
+++ greetingApp/src/components/greeting.jsx
@@ -1,36 +1,37 @@
 import "../index.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Greeting() {
-    const [greet, setGreet] = React.useState({
-        label: "",
-        index: 0,
-        hello: ["Hello", "Hi", "Hey"]
-    });
+  const [greet, setGreet] = useState({
+    index: 0,
+    hello: ["Hello", "Hi", "Hey"],
+  });
 
-const handleClick = () => {
-    const nextIndex = greet.index === greet.hello.length - 1? 0 : greet.index + 1;
+  const handleClick = () => {
+    const nextIndex =
+      greet.index === greet.hello.length - 1 ? 0 : greet.index + 1;
     setGreet({
-        ...greet, 
-        index: nextIndex,
+      ...greet,
+      index: nextIndex,
     });
-}
+    localStorage.setItem("index", nextIndex);
+  };
 
-const handleLabel = (e) => {
-    setGreet({
-        ...greet, 
-        label: e.target.value,
-    });
-}
+  useEffect(() => {
+    const item = localStorage.getItem("index")
+    if(item){
+        setGreet({...greet, 
+            index:Number(item)});
+    }
+  })
 
   return (
     <>
-        <h3>
-            Hello there! I'm a greeter. What is your name?
-        </h3>
-        <input type="input" value={greet.label} onChange={handleLabel} />
-        <h1>{greet.label ? `${greet.hello[greet.index]} !! ${greet.label}` : ""}</h1>
-        <button onClick={handleClick}>Change</button>
+      <h3>Hello there! I'm a greeter</h3>
+      <h1>
+        {greet.hello[greet.index]} Shubham
+      </h1>
+      <button onClick={handleClick}>Change</button>
     </>
   );
 }
